efe449d1950f7cf9ec1fa4a472b23dab5d5f389e
Shirley BISMUTH <U00I744@retraite.lan>
2020-10-29 16:51:30 +0100

    first commit

--- app.routes.ts
+++ app.routes.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component'
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const Routes: Routes = [
 
   
   { path: 'contact', component: ContactComponent },
   { path: '', component: HomeComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '', component: HomeComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'userprofile', component: UserProfileComponent },
 ]
 
 export { Routes };
